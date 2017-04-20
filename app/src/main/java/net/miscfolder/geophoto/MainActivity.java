b57b6d0e82f0e7163bfaca181c62f9fb277aee16
ndm_13 <ndm0113@gmail.com>
2017-04-20 11:08:15 -0400

    Implemented photo capture  - Ported some code from my camera activity demo  - MainActivity#capturePhoto(View) now starts an image capture Intent  - Implemented MainActivity#onActivityResult(int, int, Intent) to handle photo callback.  Will probably need to be replaced with a switch statement when other intents are implemented.  - Declared some more static fields  - Cleanup in AndroidManifest.xml (added permissions, formatting...)

--- app/src/main/java/net/miscfolder/geophoto/MainActivity.java
+++ app/src/main/java/net/miscfolder/geophoto/MainActivity.java
@@ -1,11 +1,19 @@
 package net.miscfolder.geophoto;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -14,9 +22,22 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
 		GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 	public static GoogleApiClient GOOGLE_API_CLIENT;
+	public static final DateFormat FILE_TIMESTAMP_FORMATTER =
+			new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US);
+	public static final File FILE_STORAGE_DIR =
+			Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	public static DatabaseHelper DATABASE_HELPER;
+
+	private File lastCapturedFile;
 
 	/**
 	 * onClick handler for R.id.floatingActionButton
@@ -27,7 +48,35 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	 * @param view     FloatingActionButton
 	 */
 	public void capturePhoto(View view) {
-		// TODO
+		String timeStamp = FILE_TIMESTAMP_FORMATTER.format(new Date());
+		String imageFileName = timeStamp + ".jpg";
+		lastCapturedFile = new File(FILE_STORAGE_DIR, imageFileName);
+		Uri outputFileUri = Uri.fromFile(lastCapturedFile);
+		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+		startActivityForResult(cameraIntent, 1);
+	}
+
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Handle camera activity result
+		if (requestCode == 1 && resultCode == RESULT_OK &&
+				lastCapturedFile != null && lastCapturedFile.length() > 0) {
+			// Generate and save photo
+			GeoPhoto photo = new GeoPhoto(lastCapturedFile.getAbsolutePath(), GOOGLE_API_CLIENT);
+			photo.save(DATABASE_HELPER);
+			// Update library
+			Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+			Uri contentUri = Uri.fromFile(lastCapturedFile);
+			mediaScanIntent.setData(contentUri);
+			getApplicationContext().sendBroadcast(mediaScanIntent);
+			// TODO refresh RecyclerView
+		}else{
+			Toast.makeText(this, "Camera didn't return an image!", Toast.LENGTH_SHORT).show();
+			if(lastCapturedFile.exists()) lastCapturedFile.delete();
+			lastCapturedFile = null;
+		}
 	}
 
 
@@ -44,6 +93,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 					.addApi(LocationServices.API)
 					.build();
 		}
+
+		// Create the database helper
+		DATABASE_HELPER = new DatabaseHelper(this);
 	}
 
 	@Override
@@ -64,7 +116,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	public void onMapReady(GoogleMap googleMap) {
 		// TODO initialize map
 	}
-
 
 	@Override
 	public void onConnected(@Nullable Bundle bundle) {
