d70de61692342e15a1ef0582c33c9647a66b4129
dgh12013 <dgh1@pct.edu>
2017-04-24 17:57:35 -0400

    Add files via upload

--- GeoPhotoClickListenerFactory.java
+++ GeoPhotoClickListenerFactory.java
@@ -1,11 +1,19 @@
 package net.miscfolder.geophoto;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import static android.view.View.*;
 
@@ -16,10 +24,10 @@ import static android.view.View.*;
  * - onShareClickListener: runs when the share button is clicked.
  * - onDeleteClickListener: runs when the delete button is clicked.
  */
-public class GeoPhotoClickListenerFactory {
+public class GeoPhotoClickListenerFactory extends FragmentActivity {
 	private final GeoPhotoRecycleViewAdapter.ViewHolder viewHolder;
 
-	public GeoPhotoClickListenerFactory(GeoPhotoRecycleViewAdapter.ViewHolder viewHolder){
+	public GeoPhotoClickListenerFactory(GeoPhotoRecycleViewAdapter.ViewHolder viewHolder) {
 		this.viewHolder = viewHolder;
 	}
 
@@ -35,13 +43,20 @@ public class GeoPhotoClickListenerFactory {
 		public void onClick(View v) {
 			// TODO switch to map and focus on location
 
-		}
-	};
+			Fragment switchMap = new Fragment();
+			FragmentManager fragmentManager = getFragmentManager();
+			fragmentManager.beginTransaction()
+					.replace(R.id.map, switchMap)
+					.commit();
 
-	public final OnClickListener onShareClickListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			// TODO complete image sharing intent
+		}
+
+
+
+		public final OnClickListener onShareClickListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO complete image sharing intent
 			/* Not sure how to find the image to be sent.
 			Intent shareIntent = new Intent();
 			shareIntent.setAction(Intent.ACTION_SEND);
@@ -49,30 +64,31 @@ public class GeoPhotoClickListenerFactory {
 			shareIntent.setType("image/*);
 			startActivity(Intent.createChooser(shareIntent, "Share image"));
 			*/
-		}
-	};
+			}
+		};
 
-	public final OnClickListener onDeleteClickListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			// TODO fix up prompt to confirm and delete file
+		public final OnClickListener onDeleteClickListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO fix up prompt to confirm and delete file
 			/*
-			AlertDialog.Builder confirmDelete = new AlertDialog.Builder();
-			confirmDelete.setTitle("Delete file");
-			confirmDelete.setMessage("Are you sure you want to delete?");
-			confirmDelete.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO delete file
-				}
-			});
-			confirmDelete.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					//Close
-					dialog.cancel();
-				}
-			});
-			confirmDelete.show();
+				AlertDialog.Builder confirmDelete = new AlertDialog.Builder();
+				confirmDelete.setTitle("Delete file");
+				confirmDelete.setMessage("Are you sure you want to delete?");
+				confirmDelete.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO delete file
+					}
+				});
+				confirmDelete.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						//Close
+						dialog.cancel();
+					}
+				});
+				confirmDelete.show();
 			*/
-		}
+			}
+		};
 	};
 }
